f0fbdefbaac55772edfc3b95aec6d6057a0328d7
Ikhlasul Akmal Hanif <88123202+IkhlasulHanif@users.noreply.github.com>
2023-08-27 15:22:34 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+
+model = load_model('./model.h5')
+
+st.title('Fire Detection Image')
+
+uploaded_file = st.file_uploader("Choose an image: ", type="jpg")
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    img = img.resize((256, 256))  
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    if st.button('Predict'):
+        prediction = model.predict(image_batch)
+        predicted_class_index = np.argmax(prediction)
+        class_labels = {0: 'COVID19', 1: 'NORMAL', 2: 'PNEUMONIA', 3: 'TURBERCULOSIS'}
+        predicted_class_label = class_labels[predicted_class_index]
+        st.write(predicted_class_label)
